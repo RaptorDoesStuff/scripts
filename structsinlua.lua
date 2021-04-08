8f155fe7ba1b41a5527af434a4422f4d39729174
RaptorDoesStuff <56210552+RaptorDoesStuff@users.noreply.github.com>
2021-04-07 22:45:50 -0600

    Update structsinlua.lua

--- structsinlua.lua
+++ structsinlua.lua
@@ -1,7 +1,7 @@
 struct = function(tab)
     setmetatable(tab,{
         __newindex = function(s,i,v)
-            if s[i] == nil and not s["_WRITABLE"] then
+            if s[i] == nil and not s._WRITABLE then
                 error("Attempted to create new value in struct",2)
                 return
             end
